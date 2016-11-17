c09616eeffd2e5a617de9b1c5abf0a36230f879e
Nacho <naxo.mcs@gmail.com>
2016-11-17 00:12:17 -0300

    Se le agrega funcionalidad al boton Se envia Texto de un planiText de una vista a la otra

--- app/src/main/java/ignacioaravena/viciusgamer/Home.java
+++ app/src/main/java/ignacioaravena/viciusgamer/Home.java
@@ -3,24 +3,40 @@ package ignacioaravena.viciusgamer;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class Home extends AppCompatActivity {
 
     Button btn_buscar;
+    EditText search;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        search=(EditText)findViewById(R.id.search);
         btn_buscar=(Button)findViewById(R.id.btn_buscar);
     }
 
-    public void Buscar(View view){
+    public void buscar(View view){
+
+        System.out.println("click boton buscar");
+        String busqueda;
+        //TODO: validar variable vacia
+        busqueda = search.getText().toString();
+        System.out.println("busqueda:"+ busqueda);
+
         Intent i = new Intent(this,Resultado.class);
+        i.putExtra("busqueda",busqueda);
         startActivity(i);
+
+
     }
 
     public void hola (View view){
--- app/src/main/java/ignacioaravena/viciusgamer/Resultado.java
+++ app/src/main/java/ignacioaravena/viciusgamer/Resultado.java
@@ -1,13 +1,37 @@
 package ignacioaravena.viciusgamer;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+import org.json.JSONObject;
+import org.w3c.dom.Text;
 
 public class Resultado extends AppCompatActivity {
+
+
+    Bundle bundle;
+    String busqueda;
+    TextView txt_busqueda;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultado);
+
+        txt_busqueda=(TextView)findViewById(R.id.txt_busqueda);
+        System.out.println(txt_busqueda.getText());
+        bundle= getIntent().getExtras();
+        busqueda=bundle.getString("busqueda");
+        System.out.println(busqueda);
+        txt_busqueda.setText(busqueda);
     }
+
+
+
 }
