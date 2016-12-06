b7e177ea3d3d2427b4416f09178326de7b3e5e86
Nacho <naxo.mcs@gmail.com>
2016-12-05 21:23:13 -0300

    Cambio de color del ActionBar

--- app/build/generated/source/r/debug/ignacioaravena/viciusgamer/R.java
+++ app/build/generated/source/r/debug/ignacioaravena/viciusgamer/R.java
@@ -2488,7 +2488,8 @@ containing a value of this type.
     }
     public static final class mipmap {
         public static final int ic_launcher=0x7f030000;
-        public static final int viciusgamer=0x7f030001;
+        public static final int ic_launchers=0x7f030001;
+        public static final int viciusgamer=0x7f030002;
     }
     public static final class string {
         public static final int abc_action_bar_home_description=0x7f060000;
